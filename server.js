0b059412f16e340ff7929940e46d3fff962168bd
PaulGimay <81556930+PaulGimay@users.noreply.github.com>
2022-02-01 10:53:21 +0100

    first commit Paul

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import routes from './routes/routes.js'
+import dotenv from 'dotenv'
+
+// Pour lancer dotenv
+dotenv.config()
 
 const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+
+mongoose.connect(process.env.MONGODB);
 
 app.listen(PORT, () => console.log(`Server listening on port : ${PORT}`))
