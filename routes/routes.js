9bdeb3eecdc1cdb7f7d9e1d9f85e6e5dd15b8f7b
PaulGimay <81556930+PaulGimay@users.noreply.github.com>
2022-02-02 12:29:35 +0100

    Versioning fixed Paul

--- routes/routes.js
+++ routes/routes.js
@@ -4,24 +4,10 @@ import { getUsers, addUser, updateUser, deleteUser } from '../controllers/userCo
 
 const router = express.Router()
 
-router.get('/', (_, res) => {
-	res.send('Home page')
-})
-
-
-
 router.get('/users', catchErrors(getUsers))
 router.post('/users', catchErrors(addUser))
 router.patch('/users/:id', catchErrors(updateUser))
 router.delete('/users/:id', catchErrors(deleteUser))
-
-router.route('/users/:id')
-	.patch((req, res) => {
-		res.send('Update user')
-	})
-	.delete((req, res) => {
-		res.send('Delete user')
-	})
 
 export default router
 
