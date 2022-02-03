5d8b4064c9d558097e65d651cd31e9aa6dd85d1f
= <LHP6.arazzok@gmail.com>
2022-02-03 15:37:02 +0100

    beautify server.js

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import jwt from 'jsonwebtoken'
 import routes from './routes/routes.js'
 
-
-// Pour lancer dotenv
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
@@ -22,14 +19,6 @@ app.use(express.json())
 
 mongoose.connect(process.env.MONGODB)
 
-app.use('/', (req, res) => {
-	const accessToken = jwt.sign('auth-token', process.env.AUTH_TOKEN)
-	res.json({ accessToken: accessToken })
-})
-
 app.use('/api/v1', routes)
-
-
-mongoose.connect(process.env.MONGODB)
 
 app.listen(PORT, () => console.log(`Server listening on port : ${PORT}`))
