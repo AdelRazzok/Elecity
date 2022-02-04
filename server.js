5e65e0392d3df6c5a9806581912d1390049d2e75
= <LHP6.arazzok@gmail.com>
2022-02-04 11:27:14 +0100

    add cors import in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import passport from 'passport'
 import routes from './routes/routes.js'
 
