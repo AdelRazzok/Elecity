98892ab171f9eb348814394c64e5dd5a27c92321
PaulGimay <81556930+PaulGimay@users.noreply.github.com>
2022-02-04 17:02:21 +0100

    Swagger

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express'
+
 import swaggerJsDoc from 'swagger-jsdoc'
 import swaggerUI from 'swagger-ui-express'
+
+import YAML from 'yamljs'
+
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import routes from './routes/routes.js'
@@ -9,20 +13,30 @@ import routes from './routes/routes.js'
 // Pour lancer dotenv
 dotenv.config()
 
+const swaggerDocument = YAML.load('./swagger.yaml')
+
+
 const PORT = process.env.PORT || 5000
 const app = express()
 
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// app.use('/api/V1', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
-// const swaggerDocs = swaggerJsDoc(swaggerOptions)
-// console.log(swaggerDocs)
+
+
+
+
+
 
 
 app.use(express.json())
 
 mongoose.connect(process.env.MONGODB)
 
-app.use('/api/V1',routes)
+app.use('/api/V1', routes)
+
 
 
 mongoose.connect(process.env.MONGODB)
