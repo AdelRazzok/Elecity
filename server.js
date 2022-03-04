0c9667678f8af39efebf7500dc0389cc082f640b
habibHadjar <lhp6.hhadjar@gmail.com>
2022-03-04 11:00:15 +0100

    tabulation

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import swaggerUi from 'swagger-ui-express'
 import { readFile } from 'fs/promises'
 const swaggerFile = JSON.parse(
 	await readFile(
-	  new URL('./swagger-output.json', import.meta.url)
+		new URL('./swagger-output.json', import.meta.url)
 	)
 )
 
