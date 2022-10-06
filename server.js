671d35304ab99c2d837b75e09c058b0e9c3abf9e
= <LHP6.arazzok@gmail.com>
2022-10-06 12:56:40 +0200

    cors patch

--- server.js
+++ server.js
@@ -3,28 +3,24 @@ import mongoose from 'mongoose'
 import swaggerUi from 'swagger-ui-express'
 import { readFile } from 'fs/promises'
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config()
 import router from './routes/routes.js'
 import './config/passport.js'
 import { Restore_Available_Cars_CRON } from './cron.js'
 Restore_Available_Cars_CRON
 
+const swaggerFile = JSON.parse(await readFile(new URL('./swagger-output.json', import.meta.url)))
+const PORT = process.env.PORT || 5000
+const app = express()
 
-
-import cors from 'cors'
-
+app.use(express.json())
 app.use(cors({
 	origin: '*',
 	options: 'GET,POST,PATCH,DELETE',
 	allowedHeaders: 'Content-type,Authorization,token',
 	credentials: true
 }))
-
-const swaggerFile = JSON.parse(await readFile(new URL('./swagger-output.json', import.meta.url)))
-const PORT = process.env.PORT || 5000
-const app = express()
-
-app.use(express.json())
 
 mongoose.connect(process.env.MONGODB)
 
@@ -35,4 +31,3 @@ app.use('/api/v1/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.listen(PORT, () => console.log(`Server listening on port : ${PORT}`))
 
 export default app
-
